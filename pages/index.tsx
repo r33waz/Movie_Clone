5704e33a4696bed7facaa4e98a94ccd15c85128e
Riwaz Thapa <reewazthapa22@gmail.com>
2023-06-15 11:26:26 +0545

    change in index.tsx file removing some undecleared components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Login from '@/components/login'
-import Product from "@/components/product";
-import Register from '@/components/register';
 import Movvie from './Movvie';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,9 +6,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      {/* <Login/> */}
-      {/* <Product/> */}
-      {/* <Register/> */}
       <Movvie/>
     </>
   )
--- slice/people.ts
+++ slice/people.ts
@@ -1,2 +1,2 @@
-import { createSlice } from "@reduxjs/toolkit";
+
 
